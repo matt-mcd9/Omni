61f0c3b38a48bfc5c42011585ad2ff10fc503e88
zarov <nikol@HOMEDESKTOP>
2026-08-04 19:28:31 -0400

    DB implementation

--- Omni/Omni/App.xaml.cs
+++ Omni/Omni/App.xaml.cs
@@ -1,8 +1,17 @@
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Omni.Data;
 
 namespace Omni
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            using var db = new OmniDbContext();
+            db.Database.Migrate();
+        }
     }
 }
